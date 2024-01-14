b97dc0ca8781886f0878b2723768e4f6e26b15ac
Quan Do <minhquand3@gmail.com>
2024-01-14 12:12:55 -0800

    Leetcode solution 103, 215, 221, 48

---
+++ leetcode/48/48.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Solution
+class Solution {
+   public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for (int i = 0; i < n / 2; i++) {
+            swap(matrix[i], matrix[n - i - 1]);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < m; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
